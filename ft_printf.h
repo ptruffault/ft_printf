ddc386d735dd6f8cc7e52ac532760c90e07e02ad
Paul TRUFFAULT <ptruffau@e1r13p13.42.fr>
2018-01-16 16:56:55 +0100

    1er pas

---
+++ ft_printf.h
@@ -0,0 +1,9 @@
+#ifndef _FT_PRINTF_H
+# define _FT_PRINTF_H
+#include <stdio.h>
+#include <stlib.h>
+#include <unistd.h>
+#include <fctnl.h>i
+#include "libft/includes/libft.h"
+int		ft_print(char *str, ...);
+# endif
