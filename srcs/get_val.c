b945584b84172abe1772a85641d95114c0fcccd1
Paul TRUFFAULT <ptruffau@e1r10p6.42.fr>
2018-10-08 17:07:04 +0200

    make save

--- srcs/get_val.c
+++ srcs/get_val.c
@@ -20,18 +20,21 @@ static char	*get_prefix(t_param *p, char *tmp_val)
 	ret = NULL;
 	tmp = NULL;
 	p->var_len = ft_strlen(tmp_val);
-	if (p->precision != -1 && p->precision > p->var_len
-	&& (TEST_SPEC_NBR(p->spec) || p->spec == '%'))
-		ret = my_strjoin(ft_strnew_nchar('0', p->precision - p->var_len), ret);
-	p->var_len = p->var_len + ft_strlen(ret);
 	if (TEST_FLAG(p->flag, '#') || p->spec == 'p')
 		tmp = get_ox(p, tmp_val);
+	if (p->precision != -1 && p->precision > p->var_len
+	&& (TEST_SPEC_NBR(p->spec) || TEST_SPEC_W(p->spec)))
+		ret = my_strjoin(ft_strnew_nchar('0', p->precision - p->var_len), ret);
+	p->var_len = p->var_len + ft_strlen(ret);
+	
+
 	if (TEST_FLAG(p->flag, '0') && !TEST_FLAG(p->flag, '-')
 	&& (((TEST_SPEC_NBR(p->spec) || TEST_SPEC_W(p->spec))
 	&& p->precision == -1) || TEST_SPEC_CHAR(p->spec))
 	&& p->width > p->var_len)
 		ret = my_strjoin(ft_strnew_nchar('0', p->width - p->var_len -
 		(p->signe != '?' ? 1 : 0) - TEST_FLAG(p->flag, ' ')), ret);
+	
 	if (TEST_FLAG(p->flag, '#') || p->spec == 'p')
 		ret = my_strjoin(tmp, ret);
 	if (p->signe != '?')
@@ -56,7 +59,7 @@ char		*get_value(t_param *p, va_list *ap)
 	p->precision == 0 && ft_atoi(tmp_val) == 0)
 	{
 		ft_strdel(&tmp_val);
-		tmp_val = ft_strnew(0);
+		return (prefix);
 	}
 	if (*tmp_val == '-')
 	{
--- srcs/tools/get_ox.c
+++ srcs/tools/get_ox.c
@@ -18,12 +18,18 @@ char	*get_ox(t_param *p, char *tmp_val)
 
 	ret = NULL;
 	if ((p->spec != 'o' && p->spec != 'x' && p->spec != 'O' && p->spec != 'X'
-	&& p->spec != 'p') || (ft_atoi(tmp_val) != 0 && p->spec == 'x' && !
-	(ret = ft_strdup("0x\0"))) || (ft_atoi(tmp_val) != 0 && p->spec == 'X' &&
-	!(ret = ft_strdup("0X\0"))) || ((p->spec == 'o' || p->spec == 'O') &&
-	!(ret = ft_strdup("0\0")))
-	|| (p->spec == 'p' && !(ret = ft_strdup("0x"))))
+	&& p->spec != 'p'))
 		return (NULL);
-	p->var_len = p->var_len + ft_strlen(ret);
+	if (p->spec == 'p') 
+		ret = ft_strdup("0x");
+	if (ft_atoi(tmp_val) != 0) 
+	{
+		if ((p->spec == 'x' && !(ret = ft_strdup("0x\0"))) 
+		|| (p->spec == 'X' && !(ret = ft_strdup("0X\0")))
+		|| ((p->spec == 'o' || p->spec == 'O') && !(ret = ft_strdup("0\0"))))	
+			return (NULL);
+	}
+	if (p->spec != 'p')
+		p->var_len = p->var_len + ft_strlen(ret);
 	return (ret);
 }
